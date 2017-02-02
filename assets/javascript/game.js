ea155795ddb186f4be5cc1e1e80f65d3f0dd60a1
Tom Langton <tom@pulpinfo.com>
2017-02-02 11:36:11 -0500

    added reset function - working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 //global variables
 var solutionList = [
-["red","A warmy primary color"],
+["red","A warm primary color"],
 ["blue","A cool primary color"],
 ["green","A verdant primary color"],
 ["yellow", "A chicken color"]
@@ -9,30 +9,34 @@ var solution;
 var solutionHint;
 var solutionInterface;
 var solutionLetters;
-var remainingGuesses = 10;
-var guessedLettersList=[];
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var gameOver=0;
 var wins =0;
 var losses=0;
 
-//display wins/losses
-document.getElementById("wins").innerHTML = "Wins: " + wins;
-document.getElementById("losses").innerHTML = "Losses: " + losses;
+var remainingGuesses;
+var guessedLettersList;
+var gameOver;
+
+function gameSetup() {
+
+remainingGuesses = 10;
+guessedLettersList=[];
+gameOver=0;
 
  // Randomly chooses a choice from the solution list array.
  myRandom = Math.floor(Math.random() * solutionList.length);
  //insert random into solution and hint vars
- 	solution = solutionList[myRandom][0];
+ solution = solutionList[myRandom][0];
   // alert( "solution: " + solution);
-  	solutionHint = solutionList[myRandom][1];
- 	 document.getElementById("solutionHint").innerHTML = "Hint: " + solutionHint;
+  solutionHint = solutionList[myRandom][1];
+  document.getElementById("solutionHint").innerHTML = "Hint: " + solutionHint;
 
 //splits word into array
 solutionLetters = solution.split("");
-
  // alert( "solutionLetters: " + solutionLetters);
- document.getElementById("solutionArray").innerHTML = "Solution array: " + solutionLetters;
+
+ // hide when actually playing - debug only
+ // document.getElementById("solutionArray").innerHTML = "Solution array: " + solutionLetters;
 
 //build the interface element for the guessed letters
 solutionInterface = [];
@@ -40,31 +44,33 @@ for (i = 0; i < solutionLetters.length; i++) {
 	solutionInterface.push("_");
 }
 
+document.getElementById("remainingGuesses").innerHTML = "Remaining Guesses: " + remainingGuesses;
+document.getElementById("tally").innerHTML = "Wins: " + wins + " &nbsp Losses: " + losses;
+document.getElementById("solutionInterface").innerHTML = "Solution Interface: " + solutionInterface;
 
+}
+
+gameSetup();
 
 
 // THIS IS THE KEYBOARD ENTRY EVENT
 // This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
-
   // Determines which key was pressed
-  // var guessedLetter = event.key;
+  var guessedLetter = String.fromCharCode(event.keyCode).toLowerCase();
 
 // stops see keys if game over
-  if (gameOver === 1) {
-document.getElementById("solutionArray").innerHTML = "Press any key to play again.";
-// gameOver = 0;
+if (gameOver === 1 ) {
+	// document.getElementById("solutionHint").innerHTML = "Press 1 to play again.";
+	gameSetup();
 
-  }
+}
 else	{
 
 
-
-var guessedLetter = String.fromCharCode(event.keyCode).toLowerCase();
-
 // validate key against alphabet (has to be a letter to pass)
-var alphabetIndex = alphabet.indexOf(guessedLetter)
-if (alphabetIndex > -1) {
+var alphabetIndex = alphabet.includes(guessedLetter)
+if (alphabetIndex == true) {
 	// alert("alphabetIndex: " + alphabetIndex);
 
 //decriment remaining guesses
@@ -73,9 +79,12 @@ document.getElementById("remainingGuesses").innerHTML = "Remaining Guesses: " + 
 
 // game over if guesses = 0
 if (remainingGuesses == 0) {
-	document.getElementById("guessedLettersList").innerHTML = "Game Over - Loser!";
 	losses = losses + 1;
 	gameOver = 1;
+	document.getElementById("guessedLettersList").innerHTML = "Game Over - Loser!";
+	document.getElementById("solutionHint").innerHTML = "Press any key to play again.";
+	document.getElementById("tally").innerHTML = "Wins: " + wins + " &nbsp Losses: " + losses;
+	return
 }
 // alert("remainingGuesses: " + remainingGuesses);
 
@@ -88,46 +97,39 @@ for (i = 0; i < solutionLetters.length; i++) {
 		solutionInterface.splice([i] ,1,guessedLetter);
 	}
 }
-document.getElementById("solutionInterface").innerHTML = "Solution Interface: " + solutionInterface;
-
 
 //checks if guessed letter is on the guessed list.
 // if it isn't - it's added (pushed) on the the variable
-var alreadyGuessed = guessedLettersList.indexOf (guessedLetter);
-if (alreadyGuessed > -1){
+var alreadyGuessed = guessedLettersList.includes (guessedLetter);
+if (alreadyGuessed == true){
 	return
 }
 else{
 	guessedLettersList.push(guessedLetter);
 }
-document.getElementById("guessedLettersList").innerHTML = "Guessed Letters: " + guessedLettersList;
-
-
 
 // checks to see if any blanks in the solution interface - where the guesses go
 // if no blanks, all letters are filled, thus a winner
-var countSolutionBlanks = solutionInterface.indexOf ("_");
-if (countSolutionBlanks == -1) {
+var countSolutionBlanks = solutionInterface.includes ("_");
+if (countSolutionBlanks == false) {
 	document.getElementById("guessedLettersList").innerHTML = "Game Over - You win!";
+	document.getElementById("solutionHint").innerHTML = "Press any key to play again.";
 	wins = wins + 1;
-	gameOver = 1;
+	gameOver = 1
 }
 // alert("countSolutionBlanks: " + countSolutionBlanks);
-
-
-// if (remainingGuesses == 0  || countSolutionBlanks >0 ) {
-// 	gameOver = 1;
-// }
-
-document.getElementById("gameOver").innerHTML = "gameOver: " + gameOver;
-document.getElementById("wins").innerHTML = "Wins: " + wins;
-document.getElementById("losses").innerHTML = "Losses: " + losses;
 
 //end of game over condition
 }
 
 // end of alphabet check
 }
+
+document.getElementById("solutionInterface").innerHTML = "Solution Interface: " + solutionInterface;
+document.getElementById("gameOver").innerHTML = "gameOver: " + gameOver;
+document.getElementById("tally").innerHTML = "Wins: " + wins + " &nbsp Losses: " + losses;
+document.getElementById("guessedLettersList").innerHTML = "Guessed Letters: " + guessedLettersList;
+
 //END OF THE ENTRY EVENT
 }
 
